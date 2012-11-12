74b336b533344b898d0cf344d5d21d2fd4e0ad24
Colin Alworth <niloc132@gmail.com>
2012-11-12 02:03:32 -0600

    updated to 2.5.0, whitespace cleanup, and null checks to allow missing callbacks

--- rpq/src/main/java/com/colinalworth/rpq/client/impl/AbstractRequestQueueImpl.java
+++ rpq/src/main/java/com/colinalworth/rpq/client/impl/AbstractRequestQueueImpl.java
@@ -20,27 +20,27 @@ import com.google.gwt.user.client.rpc.ServiceDefTarget;
 public abstract class AbstractRequestQueueImpl implements RequestQueue {
 	private List<BatchRequest> requests = new ArrayList<BatchRequest>();
 	private List<AsyncCallback<Object>> callbacks = new ArrayList<AsyncCallback<Object>>();
-	
+
 	private String address = GWT.getModuleBaseURL() + "rpq";
-	
+
 	public void setServiceEntryPoint(String address) {
 		this.address = address;
 	}
-	
+
 	public void fire() {
 		if (requests.size() == 0) {
 			return;
 		}
-		
+
 		ServiceQueueBaseAsync service = getRealService();
 		((ServiceDefTarget)service).setServiceEntryPoint(address);
-		
+
 		// Save a local copy to close over, and reset the old for the next round
 		final List<BatchRequest> activeRequests = new ArrayList<BatchRequest>(requests);
 		final List<AsyncCallback<Object>> activeCallbacks = new ArrayList<AsyncCallback<Object>>(callbacks);
 		assert activeCallbacks.size() == activeRequests.size();
 		reset();
-		
+
 		// This should only reference the final vars above
 		// TODO consider a static class for this to avoid accidental references
 		service.batchedRequest(activeRequests, new AsyncCallback<List<BatchResponse>>() {
@@ -50,7 +50,9 @@ public abstract class AbstractRequestQueueImpl implements RequestQueue {
 				for (int i = 0; i < length; i++) {
 					BatchResponse res = result.get(i);
 					AsyncCallback<Object> callback = activeCallbacks.get(i);
-					
+					if (callback == null) {
+						continue;
+					}
 					// if exception isn't null, there was an error
 					// can't make this same check on the response, since it could be null or Void
 					if (res.getCaught() != null) {
@@ -63,12 +65,15 @@ public abstract class AbstractRequestQueueImpl implements RequestQueue {
 			public void onFailure(Throwable caught) {//only gets called if the whole thing blew up
 				int length = activeCallbacks.size();
 				for (int i = 0; i < length; i++) {
-					activeCallbacks.get(i).onFailure(caught);
+					AsyncCallback<?> callback = activeCallbacks.get(i);
+					if (callback != null) {
+						activeCallbacks.get(i).onFailure(caught);
+					}
 				}
 			}
 		});
 	}
-	
+
 	/**
 	 * Adds a call to the queue. To be called by each generated method instead of an ajax call
 	 */
@@ -77,12 +82,12 @@ public abstract class AbstractRequestQueueImpl implements RequestQueue {
 		requests.add(new BatchRequest(service, method, params));
 		callbacks.add((AsyncCallback<Object>) callback);
 	}
-	
+
 	protected void reset() {
 		requests.clear();
 		callbacks.clear();
 	}
-	
+
 	/**
 	 * Generated async interface that can handle serialization of all types expected to go
 	 * over the wire.
